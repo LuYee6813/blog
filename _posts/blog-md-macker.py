537eb8bf1f3d7164d08335af9280de9e4b1ca68b
LuYee6813 <aulaul920106@gmail.com>
2022-03-20 15:46:06 +0800

    Create blog-md-macker.py

---
+++ _posts/blog-md-macker.py
@@ -0,0 +1,24 @@
+import pathlib 
+from pathlib import Path
+
+title=input("文章名稱:")
+date=input("時間:")
+tag=input("標籤:")
+
+current_directory=pathlib.Path(__file__).parent.absolute()
+myfile = Path(f'{current_directory}\{date}-{title}.md')
+myfile.touch(exist_ok=True)
+
+f = open(myfile,'w')
+md_format=f"""---
+layout: post
+title: {title}
+date: {date}
+Author: LuYee6813
+tags: [{tag}]
+toc: true
+---"""
+f.write(md_format)
+f.close()
+
+
